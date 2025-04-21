b4afdc26b1f43769b627a0f4a64a7d78b52f59b1
shimakyohsuke <shimakyohsuke@gmail.com>
2025-04-21 19:35:15 +0900

    画像パスを変更するコマンドを追加

--- replace.mjs
+++ replace.mjs
@@ -2,12 +2,12 @@ import { replaceInFile } from "replace-in-file";
 
 const options = {
 	// 置換を実行したいファイルのパスを設定
-	files: "./assets/css/main.css",
+	files: "./docs/css/main.css",
 	// 置換対象の文字を正規表現で設定
 	from: [/\.\.\/images\//g],
 	// 置換後の文字を設定
 	to: [
-		"https://raw.githubusercontent.com/WordCampKansai/wp-theme-wck2025/main/assets/images/",
+		"https://raw.githubusercontent.com/WordCampKansai/wp-theme-wck2025/main/docs/images/",
 	],
 };
 
